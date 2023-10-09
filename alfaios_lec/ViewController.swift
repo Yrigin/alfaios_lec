81f0ed0d71cdb2d0a9128c0bbf69664360b02c1d
PetrYrigin <68337082+YriginPetr@users.noreply.github.com>
2023-10-09 18:50:15 +0300

    Initial Commit

---
+++ alfaios_lec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alfaios_lec
+//
+//  Created by Пётр Ярыгин on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
